f1a4f72dd90afa04171fa3175120e61abb9b9c15
Felipe Leiva Valenzuela <felipeleiva.everis@latam.com>
2021-01-14 21:14:35 -0300

    primer commit

---
+++ src/app/pages/main.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { MainComponent } from './main.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    MainComponent
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    MainComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ]
+})
+export class MainModule { }
