c6f6d2343f00f7b8f228e09895a00e799518f6c0
Dajan Medina <dajanmedina.everis@latam.com>
2021-02-05 22:15:45 -0300

    agregamos un @input

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,27 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-incrementador',
-  templateUrl: './incrementador.component.html',
-  styles: [
-  ]
+	selector: 'app-incrementador',
+	templateUrl: './incrementador.component.html',
+	styles: [
+	]
 })
 export class IncrementadorComponent {
 
-  progreso: number = 50;
+	/** si tenemos una variabe implementada con el mismo nombre
+	 * Podemos renombrar la variable asi
+	 *  @Input('valor') progreso: number = 50;
+	 */
+	@Input() progreso: number = 50;
 
-  get getProcentaje(){
-    return `${this.progreso}%`;
-  }
 
-  cambiarValor(valor: number){
-    if(this.progreso >= 100 && valor >= 0 ){
-      return this.progreso = 100;
-    }
+	cambiarValor(valor: number) {
+		if (this.progreso >= 100 && valor >= 0) {
+			return this.progreso = 100;
+		}
 
-    if(this.progreso <= 0 && valor < 0 ){
-      return this.progreso = 0;
-    }
-    this.progreso = this.progreso + valor;
-  }
+		if (this.progreso <= 0 && valor < 0) {
+			return this.progreso = 0;
+		}
+		this.progreso = this.progreso + valor;
+	}
 }
