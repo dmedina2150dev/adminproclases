d5d14027349db02d98ff2884a503c3fdba2bee51
Dajan Medina <dajanmedina.everis@latam.com>
2021-02-05 22:28:45 -0300

    agregamos un @Output

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
 	selector: 'app-incrementador',
@@ -13,16 +13,23 @@ export class IncrementadorComponent {
 	 *  @Input('valor') progreso: number = 50;
 	 */
 	@Input() progreso: number = 50;
-
+	/** si tenemos una variabe implementada con el mismo nombre
+	 * Podemos renombrar la variable asi
+	 *  @Output('valor')valorSalida: EventEmitter<number> = new EventEmitter();
+	 */
+	@Output() valorSalida: EventEmitter<number> = new EventEmitter();
 
 	cambiarValor(valor: number) {
 		if (this.progreso >= 100 && valor >= 0) {
+			this.valorSalida.emit(100);
 			return this.progreso = 100;
 		}
 
 		if (this.progreso <= 0 && valor < 0) {
+			this.valorSalida.emit(0);
 			return this.progreso = 0;
 		}
+		this.valorSalida.emit( this.progreso );
 		this.progreso = this.progreso + valor;
 	}
 }
