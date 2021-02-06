38ed873268e2e66716aabef972a349fce8822099
Dajan Medina <dajanmedina.everis@latam.com>
2021-02-06 12:02:03 -0300

    extraemos el componente de la grafica

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
+// modulos externos
+import { ChartsModule } from 'ng2-charts';
+
+// componentes
 import { IncrementadorComponent } from './incrementador/incrementador.component';
+import { DonaComponent } from './dona/dona.component';
 
 
 
 @NgModule({
   declarations: [
-    IncrementadorComponent
+    IncrementadorComponent,
+    DonaComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ChartsModule
   ],
   exports: [
-    IncrementadorComponent
+    IncrementadorComponent,
+    DonaComponent
   ]
 })
 export class ComponentsModule { }
